0c49327a210d107b1b6a78edb3e2a2b01ff4cb82
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-15 13:28:03 -0700

    df

--- rng.py
+++ rng.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import random
 
-num1 = random.randrange(1, 4)
-num2 = random.randrange(5, 7)
+num1 = random.randrange(1, 5)
+num2 = random.randrange(5, 8)
 
 st.title("Welcome to the Random Number Guessing Game")
 
 txt_guess1 = int(st.text_input("Enter a number between 1 to 4: ", 1))
 txt_guess2 = int(st.text_input("Enter a number between 5 to 7: ", 5))
-
-btn_restart = st.button("Reset")
 
 btn_guess = st.button("Submit guess")
 if btn_guess:
@@ -20,7 +18,7 @@ if btn_guess:
         html_str1 = f""" <h1 style ="font size = 16; text_align: left; color: #FF4433;">Sorry, please try again</h1> """
         st.markdown(html_str1, unsafe_allow_html=True)
 
+st.write("The correct number are: ", num1, "and", num2)
 
-btn_reveal = st.button("Reveal answer")
-if btn_reveal:
-    st.write("The correct number are: ", num1, "and", num2)
+with st.expander("Help"):
+    st.write("To play the game, please enter your guesses in the textboxes. Press submit guess when you are ready to guess.")
