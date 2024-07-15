e1f74e4fcf418b4d7fb249add9c6f23d1482bace
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-15 13:13:16 -0700

    Update rng.py

--- rng.py
+++ rng.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import random
 
-counter = 0
-num = random.randrange(1, 10)
+num1 = random.randrange(1, 4)
+num2 = random.randrange(5, 7)
 
 st.title("Welcome to the Random Number Guessing Game")
 
-txt_guess = int(st.text_input("Enter a number between 1 to 10: ", 1))
+txt_guess = int(st.text_input("Enter a number between 1 to 4: ", 1))
+txt_guess2 = int(st.text_input("Enter a number between 5 to 7: ", 1))
 
 btn_restart = st.button("Reset")
 
 btn_guess = st.button("Submit guess")
 if btn_guess:
-    if txt_guess == num:
+    if txt_guess1 == num1 or txt_guess2 == num 2:
         st.write("You have guessed the correct number")
         st.balloons();
     else:
-        html_str1 = f""" <h1 style ="text_align: left; color: #FF4433;">Sorry, please try again</h1> """
+        html_str1 = f""" <h1 style ="font size = 16; text_align: left; color: #FF4433;">Sorry, please try again</h1> """
         st.markdown(html_str1, unsafe_allow_html=True)
-        counter = counter + 1
 
 
 btn_reveal = st.button("Reveal answer")
 if btn_reveal:
-    st.write("The correct number is: ", num)
+    st.write("The correct number are: ", num1, "and", num2)
