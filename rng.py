45de0c29fdb48515ae5d8c853aa7ffac20d361f9
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-15 14:14:15 -0700

    fixed answer display

--- rng.py
+++ rng.py
@@ -23,18 +23,18 @@ if option == "1-4":
         else:
             html_str1 = f""" <h1 style ="font-size = 16px; text_align: left; color: #FF4433;">Sorry, please try again</h1> """
             st.markdown(html_str1, unsafe_allow_html=True)
+    st.write("The correct number is: ", num1)
 elif option == "5-7":
     txt_guess2 = int(st.text_input("Enter a number between 5 to 7: ", 5))
     btn_guess = st.button("Submit guess")
     if btn_guess:
         if (txt_guess2 == num2):
             st.write("You have guessed the correct number")
-            st.balloons();
+            st.balloons()
         else:
             html_str1 = f""" <h1 style ="font-size = 16px; text_align: left; color: #FF4433;">Sorry, please try again</h1> """
             st.markdown(html_str1, unsafe_allow_html=True)
-
-st.write("The correct number are: ", num1, "and", num2)
+    st.write("The correct number is: ", num2)
 
 with st.expander("Help"):
     st.write("To play the game, please enter your guesses in the textboxes. Press submit guess when you are ready to guess.")
